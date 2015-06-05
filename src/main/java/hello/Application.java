92262effb63298f83b534bb08fb1231817086db0
NewbizIndistries <newbiz.indistries@tigerflanke.de>
2015-06-05 11:32:55 +0200

    adding health-check resource

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,12 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import static org.springframework.http.HttpStatus.OK;
 
 
 @RestController
@@ -31,26 +32,32 @@ public class Application {
 		} catch (RestClientException | IllegalArgumentException e) {
 			return "Fehler beim Zugriff auf Time API";
 		}
-		if(responseEntity.getStatusCode()!=HttpStatus.OK){
+		if (responseEntity.getStatusCode() != OK) {
 			return "Die Time API konnte die Anfrage nicht korrekt bearbeiten.";
 		}
+
 		final CurrentDate currentDate = responseEntity.getBody();
 		return String.format("Heute haben wir den %s. Aktuelle Uhrzeit: %s.", currentDate.getDate(), currentDate.getTime());
 	}
 
 	@RequestMapping(value = "/time", produces = "application/json")
 	public ResponseEntity<CurrentDate> timestamp() {
-		return new ResponseEntity<>(new CurrentDate(), HttpStatus.OK);
+		return new ResponseEntity<>(new CurrentDate(), OK);
+	}
+
+	@RequestMapping("/health")
+	public ResponseEntity<String> health() {
+		return new ResponseEntity<>("I'm fine.", OK);
 	}
 
 	public static void main(final String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	private String urlOfTimeApi(){
-		if(environment.containsProperty(PROPERTY_TIME_API)){
+	private String urlOfTimeApi() {
+		if (environment.containsProperty(PROPERTY_TIME_API)) {
 			return environment.getProperty(PROPERTY_TIME_API);
-		}else{
+		} else {
 			return DEFAULT_TIME_API_URL;
 		}
 	}
