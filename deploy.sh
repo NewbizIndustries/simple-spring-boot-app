9b96459ee0488cfd1266a65bc6866b5cfe08e133
Johannes Schmidt <johannes.schmidt@immobilienscout24.de>
2015-06-11 11:01:27 +0200

    starting elasticMQ-container

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,15 @@ IMAGE=simple-spring-boot-app
 
 mvn package docker:build
 
+docker inspect elasticmq > /dev/null 2>&1
+if [ $? == 1 ]; then
+    echo starting elasticmq server
+    docker run -d --name elasticmq visenze/elasticmq
+fi
+
 docker inspect ${IMAGE} > /dev/null 2>&1
 if [ $? == 0 ]; then
 	docker rm -f ${IMAGE}
 fi
 
-docker run --name ${IMAGE} -p 8080:8080 -t immobilienscout24/simple-spring-boot-app
+docker run --name ${IMAGE} -p 8080:8080 --link elasticmq:sqs -e "sqs.local.host=sqs" -e "sqs.local.port=80" -t immobilienscout24/simple-spring-boot-app
